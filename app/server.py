8912ea1c32d0a2b80fd48222c084883a1ce4bb01
Prashant Srinivasan <48135224+srinivasanprashant@users.noreply.github.com>
2019-12-09 17:13:32 -0700

    Update server.py
    Edit the file server.py inside the app directory and update the export_file_url variable with the URL copied above.
    In the same file, update the line classes = ['black', 'grizzly', 'teddys'] with the classes you expect from your model.

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-0eFcRdC8Daf6nsWThwgjGKIMSWaw05r'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['bridge', 'childs', 'downwarddog', 'mountain', 'plank', 'seatedforwardbend', 'tree', 'trianglepose', 'warrior1', 'warrior2']
 path = Path(__file__).parent
 
 app = Starlette()
